71035ba8afdd6720cd609080defa003a51e4d66d
Thanvi0106 <161621800+Thanvi0106@users.noreply.github.com>
2024-05-03 10:53:28 +0530

    Add files via upload

---
+++ week11b6.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[15];
+int i,j,k,n,c=0,pos=0;
+printf("\n enter the no of bits");
+scanf("%d",&n);
+printf("enter the array elements");
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=0;i<n;i++)
+{
+if(a[i]==1)
+{
+c++;
+if(c==5)
+{
+pos=1+i;
+c=0;
+for(j=n;j>=pos;j--)
+{
+k=j+1;
+a[k]=a[j];
+}
+a[pos]=0;
+n=n+1;
+}
+}
+else
+c=0;
+}
+printf("\n data after stuffing");
+printf("01111110");
+for(i=0;i<n;i++)
+{
+printf("%d",a[i]);
+}
+printf("01111110");
+getch();
+}
+
